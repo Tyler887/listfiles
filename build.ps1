220f6b802971dfa74ec5fe08dc9f427d536092dd
Tyler887 <tylermageeshields@gmail.com>
2022-01-03 10:21:05 +0000

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@ cd C:\
 echo "ListFiles v1.0"
 echo "Licensed under MS-PL"
 dir C:\
-dir $ENV:currentuser
+dir $ENV:currentuser/Downloads
 dir C:\Windows
 dir C:\Program Files
 dir C:\Program Files (x86)
