4f2be3497ecd665f9d87d864ef55def2cd2c8b91
Tyler887 <tylermageeshields@gmail.com>
2022-03-25 18:21:34 +0000

    Update app.ps1

--- app.ps1
+++ app.ps1
@@ -1,14 +1,5 @@
-echo off
-
-cd C:\
-
 echo "ListFiles v1.0"
 echo "Licensed under MS-PL"
-dir C:\
-dir %USERPROFILE%\Downloads
-dir C:\Windows
-dir C:\Program Files
-dir C:\Program Files (x86)
-dir C:\Windows\System32
+dir *
 
 echo "END OF LIST"
